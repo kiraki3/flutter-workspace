1784271dce668a14c704287ef0637e6b0f3e2844
kiraki3 <chukm.dev@gmail.com>
2024-08-27 15:44:35 +0900

    create IconButton #4.4

--- toonflix3/lib/main.dart
+++ toonflix3/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// ignore_for_file: prefer_const_constructors
 
 void main() {
   runApp(const App());
@@ -13,40 +12,73 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          textTheme: TextTheme(
-              titleLarge: TextStyle(
-        color: Colors.red,
-      ))),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.amber[100],
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
-            ],
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            showTitle ? const MyLargeTitle() : const Text('nothing'),
+            IconButton(
+              onPressed: toggleTitle,
+              icon: const Icon(Icons.remove_red_eye),
+            ),
+          ],
+        )),
       ),
     );
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'My Large Title',
       style: TextStyle(
-          fontSize: 30, color: Theme.of(context).textTheme.titleLarge!.color),
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge!.color,
+      ),
     );
   }
 }
