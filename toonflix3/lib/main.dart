81709defb4c1e017f20d838a2efccd99057f6631
kiraki3 <chukm.dev@gmail.com>
2024-08-27 11:55:30 +0900

    init toonflix3 project #4.3

---
+++ toonflix3/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: TextTheme(
+              titleLarge: TextStyle(
+        color: Colors.red,
+      ))),
+      home: Scaffold(
+        backgroundColor: Colors.amber[100],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              MyLargeTitle(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge!.color),
+    );
+  }
+}
