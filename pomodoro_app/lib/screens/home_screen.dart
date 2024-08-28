561e93a356623703dd2edf82526755654f72f2a1
kiraki3 <chukm.dev@gmail.com>
2024-08-28 13:31:21 +0900

    Implement Timer Button

--- pomodoro_app/lib/screens/home_screen.dart
+++ pomodoro_app/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500; // 총 25분
+  late Timer timer; // 당장 초기화 하지 않아도 됨, property를 사용하기 전에 반드시 초기화
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  // start count function
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    ); // 주기마다 함수를 실행
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +37,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                '$totalSeconds',
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -34,7 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: IconButton(
               iconSize: 110,
               color: Theme.of(context).cardColor,
-              onPressed: () {},
+              onPressed: onStartPressed,
               icon: const Icon(Icons.play_circle_outline),
             )),
           ),
@@ -46,6 +64,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40)),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
