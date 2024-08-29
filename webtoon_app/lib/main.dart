226564e07e7d4f4576a1dabe9ab8185a8e78e8a6
kiraki3 <chukm.dev@gmail.com>
2024-08-29 14:06:15 +0900

    Create App Bar Text Style #6.1

--- webtoon_app/lib/main.dart
+++ webtoon_app/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({super.key}); // 위젯의 key 를 stateless widget 이라는 슈퍼클래스에 보냄
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
---
+++ webtoon_app/lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+}
