c518978f97ff94312121905107baf77d2caa7524
kiraki3 <chukm.dev@gmail.com>
2024-10-31 17:54:59 +0900

    Fix potential initialization issues by ensuring Flutter binding is initialized

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -3,5 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/app/todo_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const ProviderScope(child: TodoApp()));
 }
